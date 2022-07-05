60a71f0aed4abafd2dd9e670dcffd3cac9f3b912
leprofessionnel79 <jannblomondo@gmail.com>
2022-07-05 20:22:22 +0300

    add cash method into payment page

--- lib/pages/payment/payment_page.dart
+++ lib/pages/payment/payment_page.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app/widgets/big_text.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import '../../base/custom_button.dart';
 import '../../models/order_model.dart';
 import '../../routes/route_helper.dart';
 import '../../utils/app_constants.dart';
@@ -50,44 +53,85 @@ class _PaymentPageState extends State<PaymentPage> {
           ),
           backgroundColor: AppColors.mainColor,
         ),
-        body: Center(
+        body: Center(  // in this section u can set the cash pay method
           child: Container(
             width: Dimensions.screenWidth,
             child: Stack(
+              alignment: AlignmentDirectional.center,
               children: [
-                WebView(
-                  javascriptMode: JavascriptMode.unrestricted,
-                  initialUrl: selectedUrl,
-                  gestureNavigationEnabled: true,
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(Icons.check_circle_outline,
+                          size: 100,color: AppColors.mainColor,),
+                        SizedBox(height: Dimensions.height30,),
+                        Text(
+                          "You place the order successfully ",
+                          style: TextStyle(fontSize: Dimensions.font20),
+                        ),
+                        SizedBox(height: Dimensions.height20,),
+                        Text(
+                          "Its Cash ON Delivery ",
+                          style: TextStyle(fontSize: Dimensions.font20),
+                        ),
+                        SizedBox(height: Dimensions.height20,),
 
-                  userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13E233 Safari/601.1',
-                  onWebViewCreated: (WebViewController webViewController) {
-                    _controller.future.then((value) => controllerGlobal = value);
-                    _controller.complete(webViewController);
-                    //_controller.future.catchError(onError)
-                  },
-                  onProgress: (int progress) {
-                    print("WebView is loading (progress : $progress%)");
-                  },
-                  onPageStarted: (String url) {
-                    print('Page started loading: $url');
-                    setState(() {
-                      _isLoading = true;
-                    });
-                    print("printing urls "+url.toString());
-                    _redirect(url);
+                     SizedBox(
+                        width: Dimensions.screenWidth/2,
+                       child: Padding(padding: EdgeInsets.only(
+                        top:Dimensions.height10,
+                        right: Dimensions.height20,
+                      //left: Dimensions.height20
+                      ),
+                          child: CustomButton(buttonText: "Back to Home",onPressed:
+                          ()=> Get.offAllNamed(RouteHelper.getInitial())
+                        ,),
+                    ),
+              ),
 
-                  },
-                  onPageFinished: (String url) {
-                    print('Page finished loading: $url');
-                    setState(() {
-                      _isLoading = false;
-                    });
-                    _redirect(url);
+                      ],
+                    ),
 
-                  },
-                ),
-                _isLoading ? Center(
+
+               //-------------------- PAYPAL METHOD ----------------------------
+
+                // if you want paypal method payment uncomment WebView section and comment
+                // which above
+                // WebView(
+                //   javascriptMode: JavascriptMode.unrestricted,
+                //   initialUrl: selectedUrl,
+                //   gestureNavigationEnabled: true,
+                //
+                //   userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13E233 Safari/601.1',
+                //   onWebViewCreated: (WebViewController webViewController) {
+                //     _controller.future.then((value) => controllerGlobal = value);
+                //     _controller.complete(webViewController);
+                //     //_controller.future.catchError(onError)
+                //   },
+                //   onProgress: (int progress) {
+                //     print("WebView is loading (progress : $progress%)");
+                //   },
+                //   onPageStarted: (String url) {
+                //     print('Page started loading: $url');
+                //     setState(() {
+                //       _isLoading = true;
+                //     });
+                //     print("printing urls "+url.toString());
+                //     _redirect(url);
+                //
+                //   },
+                //   onPageFinished: (String url) {
+                //     print('Page finished loading: $url');
+                //     setState(() {
+                //       _isLoading = false;
+                //     });
+                //     _redirect(url);
+                //
+                //   },
+                // ),
+                // for paypal method you have remove "!" from _isLoading in next line
+                !_isLoading ? Center(
                   child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor)),
                 ) : SizedBox.shrink(),
               ],
